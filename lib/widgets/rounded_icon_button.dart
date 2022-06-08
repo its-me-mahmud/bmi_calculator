2577cca0933019972379eeb40eac3ebf0ddbc035
abdullah <aam.msr@gmail.com>
2022-06-08 13:52:04 +0600

    Fix: Renderflex overflow at counter button

--- lib/widgets/rounded_icon_button.dart
+++ lib/widgets/rounded_icon_button.dart
@@ -19,7 +19,7 @@ class RoundedIconButton extends StatelessWidget {
       fillColor: Styles.activeColor,
       splashColor: Styles.buttonColor,
       shape: const CircleBorder(),
-      constraints: const BoxConstraints.tightFor(height: 60, width: 60),
+      constraints: const BoxConstraints.expand(height: 60, width: 60),
       child: Icon(icon, color: Styles.white),
     );
   }
